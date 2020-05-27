6309b7053aa4e3b4119021c90d99dc66977f1e51
kadirov31000 <64045660+kadirov31000@users.noreply.github.com>
2020-05-27 23:40:19 +0100

    Update ocrs-dl.py

--- ocrs-dl.py
+++ ocrs-dl.py
@@ -5,10 +5,8 @@ from IPython.display import HTML, clear_output
 
 
 ocrs()
-clear_output()
-display(HTML("<center><h2 style=\"font-family:Trebuchet MS;color:#4f8bd6;\">Successfully get urls !</h2><br></center>"))
+
 download()
-clear_output()
-display(HTML("<center><h2 style=\"font-family:Trebuchet MS;color:#4f8bd6;\">Successfully downloaded !</h2><br></center>"))
+
 os.remove("out.txt")
 os.remove("urlinfo.txt")
